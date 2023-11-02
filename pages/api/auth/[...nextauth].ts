4470df17193d4984ebd5d32025d9ca73fad0cea2
Aaryan Kalra <aryan.smart832@gmail.com>
2023-11-03 02:21:24 +0530

    second commit

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,52 @@
+import bcrypt from "bcrypt";
+import nextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+import prisma from "@/lib/prismadb";
+import NextAuth from "next-auth/next";
+import { use } from "react";
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Credentials({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials");
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        //user doesn't exist in database
+        if (!user || !user?.hashedPassword) {
+          throw new Error("Invalid credentials");
+        }
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+        if (!isCorrectPassword) {
+          throw new Error("Invalid Credentials");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
